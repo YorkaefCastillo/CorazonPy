0d5eb43796f85ee5ca4370b18d6b95fc228e9e91
YorkaefCastillo <84946551+YorkaefCastillo@users.noreply.github.com>
2023-02-06 22:44:35 -0600

    Add files via upload

---
+++ Corazon.py
@@ -0,0 +1,11 @@
+import turtle
+turtle.color('yellow','red')
+turtle.begin_fill()
+turtle.left(60)
+turtle.forward(110)
+turtle.circle(50,170)
+turtle.left(-80)
+turtle.circle(50,170)
+turtle.forward(110)
+turtle.end_fill()
+
